16a7ae8bc2568fd1c61e682196855d32467c3b75
Spike Vinz Cruz <spikevinz@gmail.com>
2021-08-03 10:59:33 +0800

    add html and css layout

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let myLibrary = [];
 
-function Book(title, author) {
+function Book(title, author, pages) {
   this.title = title
   this.author = author
+  this.pages = pages
+  this.read = false
 }
 
 function addBookToLibrary(book) {
   myLibrary.push(book)
+}
+
+function showBooksFromLibrary() {
+  
 }
