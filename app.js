f562ae8805bd598f7f91e78388cbecd85522902d
Spike Vinz Cruz <spikevinz@gmail.com>
2021-08-04 10:00:11 +0800

    add bookshelf and book template

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ let myLibrary = [];
 const addBtn = document.querySelector('#book-btn');
 const bookForm = document.querySelector('#book-form'); 
 const formBtn = document.querySelector('#form-button');
+const bookShelf = document.querySelector('#bookshelf');
+
+let bookTemplate = `
+    <div id="book" class="bg-blue-100 rounded shadow-lg h-40 text-center p-1 flex flex-col justify-around">
+      <h1 class="text-2xl font-bold">Title</h1>
+      <p class="text-md">Author Author</p>
+      <p class="text-md">Pages: 69</p>
+    </div>
+`;
 
 function Book(title, author, pages) {
   this.title = title
@@ -28,7 +37,12 @@ function clearForm() {
 }
 
 function showBooksFromLibrary() {
-  
+  bookShelf.insertAdjacentElement('beforeend', '<div>hello</div>');
+}
+
+
+function booksFormat() {
+
 }
 
 const hideShow = () => {
