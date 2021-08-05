e9c5fd576181421a97bec648682775127fa650b1
Spike Vinz Cruz <spikevinz@gmail.com>
2021-08-05 09:16:40 +0800

    add remove book button

--- app.js
+++ app.js
@@ -31,17 +31,33 @@ function clearForm() {
 }
 
 function showBooksFromLibrary() {
+  // clear books that is not in the myLibrary Array
+  bookShelf.innerHTML = '';
+  
   myLibrary.forEach((book) => {
     
-    let bookTemplate = `<div id="book" class="bg-blue-100 rounded shadow-lg h-40 text-center p-1 flex flex-col justify-around">
+    let bookTemplate = `<div id="book" class="relative bg-blue-100 rounded shadow-lg h-40 text-center p-1 flex flex-col justify-around">
         <h1 class="text-2xl font-bold">${book.title}</h1>
         <p class="text-md">${book.author}</p>
         <p class="text-md">Pages: ${book.pages}</p>
+        <button class="absolute left-1 top-1 bg-red-500 rounded-md text-white block" id="remove-btn" >remove</button>
       </div>`;
 
     bookShelf.insertAdjacentHTML('beforeend', bookTemplate);
   });
 }
+
+
+const removeBookFromLibrary = e => {
+  if(e.target && e.target.matches("button#remove-btn")) {
+    let bookTitle = e.target.parentNode.firstElementChild.textContent;
+
+    myLibrary = myLibrary.filter( ({ title }) => title !== bookTitle);
+
+    showBooksFromLibrary();
+    console.log(myLibrary);
+  }
+};
 
 
 const hideShow = () => {
@@ -52,3 +68,9 @@ const hideShow = () => {
 
 addBtn.addEventListener('click', hideShow);
 formBtn.addEventListener('click', addBookToLibrary);
+bookShelf.addEventListener('click', removeBookFromLibrary);
+
+// seed book
+
+let test = new Book('Example', 'John Doe', 1);
+myLibrary.push(test);
